0c5608b0fa2beb083142a6a4f333bfffbba608be
JasonPC <jasonwong287962@gmail.com>
2023-01-20 01:46:21 -0600

    mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -1,16 +1,23 @@
-let array = [5,6,2,1,3,10,9,8]
+let array = [5,6,2,1,3,11,9,8]
+let array1 = [5,6,2,1,3,10]
+let array2 = [5,6,2,1,3,-10,3,2,5,]
+console.log('Orignal', array);
+console.log('Merge: :)',mergeSort(array));
 
-console.log(mergeSort(array));
+console.log('Orignal', array1);
+console.log('Merge: :)',mergeSort(array1));
+console.log('Orignal', array2);
+console.log('Merge: :)',mergeSort(array2));
+
 
 
   function mergeSort(array){
     let newArray = [];
-      if(array.length < 2 ){
+      if(array.length < 2 )
         return array;
-      }
      let arrayHolder1 = (mergeSort(array.slice(0,array.length/2))) 
      let arrayHolder2 = (mergeSort(array.slice(array.length/2)));
-if(arrayHolder1.length>1 && arrayHolder2.length>1 ){
+if(arrayHolder1.length>1 || arrayHolder2.length>1 ){
 let i = 0; let inner = 0;
   while(arrayHolder1.length != 0 && arrayHolder2.length !=0){
         if(arrayHolder1[i] < arrayHolder2[inner]){
@@ -19,14 +26,18 @@ let i = 0; let inner = 0;
         else{
           newArray.push(arrayHolder2.shift());
         }
-        if(arrayHolder2.length == 0){
-          for(i = 0; i<= arrayHolder1.length;i++)
+         if(arrayHolder1.length == 0 && arrayHolder2[0] != 'undefined'){
+          let breaks = arrayHolder2.length;
+          for(i = 0; i< breaks;i++){
+            newArray.push(arrayHolder2.shift());
+          }
+        }
+         if(arrayHolder2.length == 0 && arrayHolder1[0] != 'undefined'){
+          let breaks = arrayHolder1.length;
+          for(i = 0; i< breaks;i++)
           newArray.push(arrayHolder1.shift());
         }
-        else if(arrayHolder1.length == 0){
-          for(i = 0; i<= arrayHolder2.length;i++)
-          newArray.push(arrayHolder2.shift());
-        }
+       
       }
   return newArray;
 }
